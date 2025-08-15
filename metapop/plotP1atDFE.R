dffadf642e87b9b73a5268fd604810f771d535e0
yuyangzhang2006 <yuyangzhang2006@gmail.com>
2025-08-15 11:15:20 +0800

    update some plots

--- metapop/plotP1atDFE.R
+++ metapop/plotP1atDFE.R
@@ -28,3 +28,50 @@ plot_P1 <- function(P_func=P1_at_DFE, x_start = 1, x_end = 5, step = 0.01) {
 }
 
 plot_P1()
+
+plot_P1_2d <- function(R0_range = c(1, 5), epsilon_range = c(1e-4, 0.02),
+                       R0_steps = 100, epsilon_steps = 100) {
+
+  R0_vals <- seq(R0_range[1], R0_range[2], length.out = R0_steps)
+  eps_vals <- seq(epsilon_range[1], epsilon_range[2], length.out = epsilon_steps)
+  
+  P1_mat <- outer(R0_vals, eps_vals, Vectorize(function(R0, eps) {
+    P1_at_DFE(R0 = R0, epsilon = eps)
+  }))
+  
+  filled.contour(R0_vals, eps_vals, P1_mat,
+                 xlab = "R0",
+                 ylab = "epsilon",
+                 main = "P1 with R0 and epsilon",
+                 color.palette = terrain.colors)
+}
+
+plot_P1_2d()
+
+plot_P1_2d_logP1 <- function(R0_range = c(1, 5), epsilon_range = c(1e-4, 0.02),
+                             R0_steps = 100, epsilon_steps = 100) {
+  # create sequences
+  R0_vals <- seq(R0_range[1], R0_range[2], length.out = R0_steps)
+  eps_vals <- seq(epsilon_range[1], epsilon_range[2], length.out = epsilon_steps)
+  
+  # compute P1 matrix
+  P1_mat <- outer(R0_vals, eps_vals, Vectorize(function(R0, eps) {
+    P1_at_DFE(R0 = R0, epsilon = eps)
+  }))
+  
+  # take log10 of P1 for contour levels
+  logP1_mat <- log10(P1_mat)
+  
+  # define contour levels (example as you suggested)
+  levels <- c(-12, -8, -4, -2, -1, log10(0.2), log10(0.5), log10(0.8))
+  
+  # plot filled contour using log10(P1)
+  filled.contour(R0_vals, eps_vals, logP1_mat,
+                 levels = levels,
+                 xlab = "R0",
+                 ylab = "epsilon",
+                 main = "P1 with R0 and epsilon (log scale of P1)",
+                 color.palette = terrain.colors)
+}
+
+plot_P1_2d_logP1()
