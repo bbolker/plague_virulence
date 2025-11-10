48cf3dc563411b7aac145f4772c1134a0b87c4a5
yuyangzhang2006 <yuyangzhang2006@gmail.com>
2025-11-10 09:31:05 +0800

    sims for larger range of K

---
+++ metapop/stochastic/plot_batch2.R
@@ -0,0 +1,39 @@
+library(ggplot2); theme_set(theme_bw())
+library(tidyverse)
+
+dd <- readRDS("outputs/sim_batch2.rds")
+print(nrow(dd))
+
+pivfun <- function(x) {
+  res <- tidyr::pivot_longer(x, contains("_"))
+  if ("alphavec" %in% names(res)) {
+    res <- res |> mutate(alphavec_f =
+                           ordered(alphavec, levels = unique(alphavec), labels = signif(unique(alphavec), 2)))
+  }
+  res
+}
+
+dd_long <- pivfun(dd) |> rename(R0 = "R0vec", K = "Kvec")
+
+out_dir <- "sim_batch2_plot"
+dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
+
+# produce plot for each distinct K
+Ks <- sort(unique(dd_long$K))
+for (k in Ks) {
+  ddk <- dd_long %>% filter(K == k)
+
+  gg1 <- ggplot(ddk, aes(R0, value, colour = alphavec_f)) +
+    geom_point() +
+    ## GAM gives nicer plots than default loess smoothing, but fails in a few cases
+    ## (because quasi-eq doesn't leave enough points etc.)
+    geom_smooth(method = "gam", formula = y ~ s(x)) +
+    facet_wrap(~ name, scale = "free", ncol = 2)
+  
+  p <- gg1 + ggtitle(paste("K =", format(k, scientific = FALSE)))
+  
+  fname <- file.path(out_dir, paste0("plot_batch2_K_", format(k, scientific = FALSE), ".pdf"))
+  ggsave(fname, p, width = 10, height = 5)
+  message("Saved: ", fname)
+  
+}
---
+++ metapop/stochastic/sim_batch2.R
@@ -0,0 +1,53 @@
+#R0,alpha,K
+
+source("simulation_funs.R")
+
+fn_raw <- "sim_batch2_raw.rds"
+fn_sum <- "sim_batch2.rds"
+
+retry <- TRUE    ## checkpoint/pick up from previous runs?
+ncores <- 16
+nsim <- 120
+
+
+R0vec <- seq(1.2, 3, by = 0.1)
+alphavec <- 5 * 10^seq(-6, -3, by = 0.5)
+Kvec <- 10^(2:10)
+
+dd <- expand.grid(R0vec = R0vec,
+                  alphavec = alphavec,
+                  Kvec = Kvec)
+
+res <- list()
+start <- 1
+if (retry && file.exists(fn_raw)) {
+  res <- readRDS(fn_raw)
+  start <- length(res) + 1
+}
+
+if (start <= nrow(dd)) {
+  
+  cat("i R0 alpha K total_pops inf_patch total_inf\n")
+  for (i in start:nrow(dd)) {
+    params <- params0  ## default parameters, from simulation_funs.R
+    params[["R0"]] <- dd$R0vec[i]
+    params[["alpha"]] <- dd$alphavec[i]
+    params[["K"]] <- dd$Kvec[i]
+    
+    res[[i]] <- mult_sim_mp(nsim = nsim, ncores = ncores, params = params, seed = 100 + i)
+    cat(i, unlist(dd[i, ]), sumfun1(res[[i]]), "\n")
+    saveRDS(res, fn_raw)
+  }
+}
+
+## summarize 
+res_sum <- lapply(res, sumfun1)
+dd_out <- data.frame(dd, do.call(rbind, res_sum))
+
+## attach metadata as attributes
+attr(dd_out, "ncores") <- ncores
+attr(dd_out, "nsim") <- nsim
+attr(dd_out, "params0") <- params0
+
+saveRDS(dd_out, fn_sum)
+cat("Saved summary to", fn_sum, "\n")
