4b70ae684b33960f59fd9bb53adc825f3fca412a
Ben Bolker <bbolker@gmail.com>
2026-05-02 17:05:07 -0400

    alternative poisson plot tweaks

--- metapop/poisson/plot_alt.R
+++ metapop/poisson/plot_alt.R
@@ -1,57 +1,94 @@
-library(ggplot2); theme_set(theme_bw())
-library(tidyverse)
+library(tidyverse); theme_set(theme_bw())
+zmargin <- theme(panel.spacing = grid::unit(0, "lines"))
 library(colorspace)
+library(cowplot)
 
 dd <- readRDS("outputs/sim.rds")
-print(nrow(dd))
+stopifnot(nrow(dd) == 700)
 
 plot_vars <- c("extinction_rate", "mean_extinct_time",
                "total_pops", "infected_patches", "total_inf")
 
-dd_long <- dd %>%
-  select(R0vec, alphavec, rhovec, c0vec, rvec, all_of(plot_vars)) %>%
-  pivot_longer(cols = all_of(plot_vars), names_to = "metric", values_to = "value") %>%
+dd_long <- dd |>
+  mutate(across(c(total_pops, total_inf), ~ . / 1e6)) |>
+  select(R0vec, alphavec, rhovec, c0vec, rvec, all_of(plot_vars)) |>
+  pivot_longer(cols = all_of(plot_vars),
+               names_to = "metric") |>
   mutate(
-    metric = factor(metric, levels = plot_vars),  
+    metric = factor(metric, levels = plot_vars),
+    log10alpha = round(log10(alphavec),1),
     alphavec_f = factor(alphavec, levels = unique(alphavec),
                         labels = signif(unique(alphavec), 2))
   )
 
-param_combos <- dd %>%
-  distinct(rhovec, c0vec, rvec) %>%
-  arrange(rhovec, c0vec, rvec)
-
-pdf("outputs/plot.pdf", width = 14, height = 10)
-
+## quasi-binomial smooth (slow)
 qb_smooth <- geom_smooth(method = "gam",
                          method.args = list(family = quasibinomial),
                          alpha = 0.2)
 
+## quasi-poisson smooth (a little less slow)
 qp_smooth <- geom_smooth(method = "gam",
                          method.args = list(family = quasipoisson),
                          alpha = 0.2)
 
+## use log10-alpha (prettier for non-integer values)
 gg0 <- ggplot(dd_long,
-              aes(R0vec, value, colour = alphavec, group = alphavec)) +
+              aes(R0vec, value, colour = log10alpha, group = log10alpha)) +
   geom_point() +
+  # use guide_legend rather than guide_colourbar since we have discrete values anyway
   scale_color_continuous_sequential(palette = "Heat",
-                                    trans = "log10",
-                                    breaks = unique(dd_long$alphavec),
-                                    labels = round(log10(unique(
-                                      dd_long$alphavec)), 1))
-               
-gg0 + (dd_long |> filter(metric  == "extinction_rate")) +
-  ##  qb_smooth +
-  ## geom_smooth(method = "loess", method.args = list(span  = 0.01)) +
-  geom_line() +
-  facet_grid(alphavec~rhovec, labeller = label_both) +
-  scale_y_continuous(limits = c(0,1),
-                     oob = scales::squish) +
-  labs(title = "extinction rate")
+                                    guide = guide_legend())
 
-gg0 + (dd_long |> filter(metric  == "infected_patches")) +
-  geom_smooth() +
-  facet_grid(alphavec~rhovec, labeller = label_both) +
-  scale_y_continuous(limits=c(0,NA), oob = scales::squish)
+## if we did want a reversed colour bar (to match facet ordering top-to-bottom:
+##  guide_colorbar(reverse=TRUE)
+## https://aosmith.rbind.io/2018/01/19/reversing-the-order-of-a-ggplot2-legend/
 
+plot_fun <- function(focal_metric = "extinction_rate", add_smooth = FALSE, limits = c(0, NA),
+                     title = focal_metric) {
+  dd2 <- dd_long |> filter(metric  == focal_metric,
+                           ## restrict to 'interesting' cases
+                           log10alpha>=(-5.5),
+                           log10alpha<=(-3.5))
+  gg1 <- gg0 + dd2 +
+    geom_line() +
+    facet_grid(log10alpha~rhovec, labeller = label_both) +
+    scale_y_continuous(limits = limits,
+                       oob = scales::squish) +
+    labs(title = title) +
+    zmargin
 
+  if (add_smooth) {
+    ## https://stackoverflow.com/a/46285325/190277
+    gg1 <- gg1 + stat_smooth(geom = "line", lty = 2, alpha = 0.5, se = FALSE)
+  }
+  return(gg1)
+  
+}
+
+design <- tribble(
+  ~ focal_metric, ~add_smooth, ~title,
+  "mean_extinct_time", FALSE, "Mean Extinction Time",
+  "extinction_rate", FALSE, "Extinction Rate",
+  "total_pops", TRUE,  "Total host population (x 1e6; quasi-eq)",
+  "infected_patches", TRUE, "Infected patches (quasi-eq)",
+  "total_inf", TRUE, "Total Infections (x 1e6; quasi-eq)")
+
+plot_list <- list()
+for (i in 1:nrow(design)) {
+  plot_list[[design$focal_metric[i]]] <-
+    do.call(plot_fun, design[i,])
+}
+
+## cleanup: colour legend only on last plot
+for (i in 1:(length(plot_list)-1)) {
+  plot_list[[i]] <- plot_list[[i]] + guides(color = "none")
+}
+
+## cleanup: no y-axis labels (should apply upstream ...)
+plot_list <- lapply(plot_list, \(x) (x + labs(y="")))
+
+pdf("poisson_plot_alt.pdf", width = 20, height = 10)
+suppressWarnings(
+  plot_grid(plotlist = plot_list)
+)
+dev.off()
