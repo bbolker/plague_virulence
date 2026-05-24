155e9a188d62b74ca609d0533e64fb49e4dfc9e2
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-05-24 15:56:45 -0400

    spruceside meeting

--- metapop/odin/tests.R
+++ metapop/odin/tests.R
@@ -7,12 +7,13 @@ set.seed(101)
 run <- run_simulator_odin(mod)
 out <- conv_odin(run) |> dplyr::arrange(step, patch, state)
 
+library(ggplot2)
 gg1 <- ggplot(out, aes(step, value, colour = state)) +
   geom_line(aes(group = interaction(state, patch)))  +
   scale_y_log10()
+print(gg1)
 
-
-expect_snapshot_print(out, label = "odin_run1")
+## expect_snapshot_print(out, label = "odin_run1")
 
 source("discrete_macpan2.R")
 mod <- make_simulator_macpan2()
@@ -20,10 +21,10 @@ set.seed(101)
 run <- run_simulator_macpan2(mod)
 out <- conv_macpan2(run) |> dplyr::arrange(step, patch, state)
 
-expect_snapshot_print(out, label = "macpan2_run1")
+## expect_snapshot_print(out, label = "macpan2_run1")
 
 source("discrete_pureR.R")
 set.seed(101)
 pureR_run1 <- run_simulator_pureR()
 
-expect_snapshot_print(out, label = "pureR_run1")
+## expect_snapshot_print(out, label = "pureR_run1")
