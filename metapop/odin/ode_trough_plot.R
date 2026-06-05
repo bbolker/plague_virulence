1ecbbb8441a57bd680848091ea4785e07385643f
Ben Bolker <bbolker@gmail.com>
2026-06-05 13:42:48 -0400

    hacking ode trough plots

--- metapop/odin/ode_trough_plot.R
+++ metapop/odin/ode_trough_plot.R
@@ -7,10 +7,11 @@ res <- readRDS("outputs/ode_trough.rds")
 make_raster <- function(dat, fill_var, fill_label = fill_var, title = fill_var) {
   ggplot(dat, aes(x = log10(r), y = beta, fill = .data[[fill_var]])) +
     geom_raster() +
-    scale_fill_viridis_c(name = fill_label, na.value = "grey80") +
+      scale_fill_viridis_c(name = fill_label, na.value = "grey80",
+                           trans = "log10") +
     scale_x_continuous(expand = c(0, 0)) +
     scale_y_continuous(expand = c(0, 0)) +
-    labs(x = expression(log[10](r)), y = expression(beta), title = title)
+    labs(x = expression(log[10]('growth')), y = expression(R[0]), title = title)
 }
 
 panels <- list(
@@ -26,7 +27,9 @@ panels <- list(
 
 for (dem in c("logistic", "linear")) {
   dat <- filter(res, demography == dem)
-  plots <- lapply(panels, \(p) make_raster(dat, p$var, p$label))
+  plots <- lapply(panels, \(p) make_raster(dat, fill_var = p$var,
+                                           fill_label = "",
+                                           title =  p$label))
   pw <- wrap_plots(plots, ncol = 4) +
     plot_annotation(title = dem)
   ggsave(paste0("ode_trough_", dem, ".png"), pw, width = 16, height = 8)
